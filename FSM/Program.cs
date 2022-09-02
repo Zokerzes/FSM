9ae3ae4a319a7db3a00694551bedbc9713577b1e
Zokerzes <romzes85@mail.ru>
2022-09-02 21:16:43 +0300

    Добавьте файлы проекта.

---
+++ FSM/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace FSM
+{
+    enum state { development, examination, agreement, statement, canceled, storage };
+    public class Masine
+    {
+        state actualState;
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            state key = 0;
+            switch (key)
+            {
+                case state.development:
+                    break;
+                case state.examination:
+                    break;
+                case state.agreement:
+                    break;
+                case state.statement:
+                    break;
+                case state.canceled:
+                    break;
+                case state.storage:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
